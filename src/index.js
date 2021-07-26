87a7b54d1fd46f81747bd4c035f5b335e505f9d9
EugeneZhdanov <jeka7753191@gmail.com>
2021-07-27 00:00:58 +0300

    ADDS PRIMARY CHANGES APP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+import 'modern-normalize/modern-normalize.css';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
